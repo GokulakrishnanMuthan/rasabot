6be45104a8de6529ff6dbcadf52e3b0746eb1b68
GKTechy <glapskrish@gmail.com>
2020-03-20 18:31:33 +0530

    First Commit

---
+++ actions.py
@@ -0,0 +1,44 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/core/actions/#custom-actions/
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
+
+
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_hello_world"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message("Response from Custom Action!!!!")
+
+        return []
+
+class ActionSearchBranch(Action):
+
+    def name(self) -> Text:
+        return "action_search_branch"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        ## extract the required slots
+        location=tracker.get_slot("location")
+        print("custom action called!",location)
+        dispatcher.utter_message("Response from action_searchBranch!!!!")
+
+        return []
---
+++ data/stories.md
@@ -0,0 +1,42 @@
+## happy path
+* greet
+  - utter_greet
+
+* mood_great
+  - utter_happy
+
+## sad path 1
+* greet
+  - utter_greet
+* mood_unhappy
+  - utter_cheer_up
+  - utter_did_that_help
+* affirm
+  - utter_happy
+
+## sad path 2
+* greet
+  - utter_greet
+* mood_unhappy
+  - utter_cheer_up
+  - utter_did_that_help
+* deny
+  - utter_goodbye
+
+## say goodbye
+* goodbye
+  - utter_goodbye
+
+## bot challenge
+* bot_challenge
+  - utter_iamabot
+
+## list allBranches
+* listBranches
+  - utter_branches
+
+## search Branches near location
+* greet
+  - utter_greet
+* searchBranch{"location":"coimbatore"}
+  - action_search_branch         
